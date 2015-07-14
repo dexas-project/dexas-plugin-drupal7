053c606126700b7255c6b03fb17006e1723220cf
sidhujag <sidhujag@hotmail.com>
2015-07-14 16:25:10 -0700

    update to match latest bitsharescheckout

--- bitshares/userfunctions.php
+++ bitshares/userfunctions.php
@@ -63,7 +63,8 @@ function getOrderWithStatusFromCartHelper($id, $response_code)
               $ret = array (
                 "order_id" => $order->order_id,
 	              "currency" =>	$order->currency,
-	              "order_total" =>	$order->order_total
+	              "order_total" =>	$order->order_total,
+                "date_added" =>	$order->created
 	            );
               array_push($orders, $ret);
             }
@@ -99,7 +100,8 @@ function getOrderWithStatusFromCartHelper($id, $response_code)
           $ret = array (
             "order_id" => $id,
 	          "currency" =>	$response->currency,
-	          "order_total" =>	$response->order_total
+	          "order_total" =>	$response->order_total,
+            "date_added" =>	$response->created
 	        );
           array_push($orders, $ret);
         }
@@ -156,9 +158,9 @@ function getOpenOrdersUser()
 		$total = $responseOrder['order_total'];
 		$total = number_format((float)$total,2);		
 		$newOrder['total'] = $total;
-		$newOrder['currency_code'] = $responseOrder['currency'];
+		$newOrder['asset'] = $responseOrder['currency'];
 		$newOrder['order_id'] = $responseOrder['order_id'];
-		$newOrder['date_added'] = 0;
+		$newOrder['date_added'] = $responseOrder['date_added'];
 		array_push($openOrderList,$newOrder);    
 	}
 	return $openOrderList;
@@ -197,6 +199,7 @@ function doesOrderExistUser($memo, $order_id)
 				$order['total'] = $total;
 				$order['asset'] = $asset;
 				$order['memo'] = $memo;	
+        $order['date_added'] = $responseOrder['date_added'];
 				return $order;
 			}
 	}
